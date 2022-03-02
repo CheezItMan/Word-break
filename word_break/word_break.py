cb7439a2f48a3cb2376cd918cd6ecfb47355a196
Chris McAnally <chris@adadevelopersacademy.org>
2022-03-02 14:42:00 -0800

    solution

--- word_break/word_break.py
+++ word_break/word_break.py
@@ -3,4 +3,16 @@ from heapq import heappush, heappop
 
 
 def word_break(s: str, word_dict: Set[str]) -> bool:
-    pass
+    # Memo stores the fact that the substring ending at the given index,
+    # is in the dictionary or not.
+    memo = [False for _ in range(len(s) + 1)]
+    #  The empty string is always in the dictionary.
+    memo[0] = True
+
+    for i in range(1, len(s) + 1):
+        for j in range(i):  # from 0 to i-1
+            if memo[j] and s[j:i] in word_dict:
+                memo[i] = True
+                break
+
+    return memo[len(s)]
